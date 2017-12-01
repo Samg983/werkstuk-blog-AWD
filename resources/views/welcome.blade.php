1f1264fc11d636705e6c2b57a2ef6344663b135a
Sam <sam.goeman@hotmail.com>
2017-12-01 17:15:50 +0100

    test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,9 +79,8 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Blog Sam Goeman
                 </div>
-
                 <div class="links">
                     <a href="https://laravel.com/docs">Documentation</a>
                     <a href="https://laracasts.com">Laracasts</a>
